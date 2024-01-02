130482109054d0bf6a8cfeecfafef49e159a1306
Kamil Kuziora <kam.kuziora@gmail.com>
2024-01-02 01:43:44 +0100

    add not found page

---
+++ src/app/not-found.jsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <div className='flex min-h-[50vh] flex-col items-center justify-center'>
+      <span className='text-9xl text-gray-400'>404</span>
+      <p>Nie znaleziono takiej strony</p>
+    </div>
+  );
+};
+
+export default NotFound;
